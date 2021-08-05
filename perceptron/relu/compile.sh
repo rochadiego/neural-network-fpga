49d69281ed4838872d5882255df0ef3d962fadc2
rochadiego <diego98martin@gmail.com>
2021-08-05 13:18:03 -0300

    removing unnecessary files

--- perceptron/relu/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ghdl -a --std=08 --time-resolution=ns relu.vhd
-ghdl -a --std=08 --time-resolution=ns relu_tb.vhd
-ghdl -e --std=08 --time-resolution=ns relu_tb
-ghdl -r --std=08 --time-resolution=ns relu_tb --stop-time=80ns --vcd=relu.vcd
-
-$? || gtkwave relu.vcd && clear
--- perceptron/sigmoid/approximation2/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ghdl -a --std=08 --time-resolution=ns sigmoid.vhd
-ghdl -a --std=08 --time-resolution=ns sigmoid_tb.vhd
-ghdl -e --std=08 --time-resolution=ns sigmoid_tb
-ghdl -r --std=08 --time-resolution=ns sigmoid_tb --stop-time=80ns --vcd=sigmoid.vcd
-
-$? || gtkwave sigmoid.vcd && clear
